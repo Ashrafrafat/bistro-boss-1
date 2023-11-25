5e34fa0295f6d65497a7ee180514609f53f8457a
Ashraf Rafat <ashrafuddinrafat@gmail.com>
2023-11-24 19:39:37 -0500

    Update Cart.jsx

--- src/Components/SectionTitle/Cart/Cart.jsx
+++ src/Components/SectionTitle/Cart/Cart.jsx
@@ -9,7 +9,7 @@ return (
                 <h2 className="card-title">{name}</h2>
                 <p>{recipe}</p>
                 <div className="card-actions justify-end">
-                    <button onClick={() => handleAddToCart(item)} className="btn btn-outline bg-slate-100 border-0 border-b-4 border-orange-400 mt-4">Add to Cart</button>
+                    <button className="btn btn-outline bg-slate-100 border-0 border-b-4 border-orange-400 mt-4">Add to Cart</button>
                 </div>
             </div>
         </div>
